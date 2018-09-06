a73ffa8ce4bf617116a1f162b1ecdbbac5d10db6
Durril Weller <dzcw12@gmail.com>
2018-09-06 22:36:30 +0100

    Day 1
    Added chickens

---
+++ Zenva TextRPG/Assets/Scripts/Uber Chicken.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TextRPG
+{
+    public class ViciousChicken : Enemy
+    {
+
+        // Use this for initialization
+        void Start()
+        {
+            Energy = 15;
+            Attack = 8;
+            Defence = 5;
+            Gold = 30;
+            Inventory.Add("Sniper");
+        }
+
+
+    }
+}
